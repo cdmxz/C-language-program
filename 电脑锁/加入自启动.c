54ad756def48ce9da2cd01e2a16079e14d05e38a
cdmxz <3428212185@qq.com>
2019-11-17 14:31:22 +0800

    update

--- 电脑锁/加入自启动.c
+++ 电脑锁/加入自启动.c
@@ -1,49 +1,62 @@
 #include <stdio.h>
 #include <windows.h>
 #include <direct.h>
-#define MAXPATH 1024
+#define MAXPATH 512
+
+void copyfile(char *fileread, char *filewrite);
+void flush(void);
 
 int main(void)
 {
+	while(1)
+	{
+	system("title 添加自启动 && mode con cols=35 lines=10 && color 07"); 
 	char *szSubKey = { "Software\\Microsoft\\Windows\\CurrentVersion\\Run" }; //系统启动项路径
-	char copy[MAXPATH] = { "copy \"" };//copy命令复制文件
+	char *del = { "Software\\Microsoft\\Windows\\CurrentVersion\\Run\\电脑锁" };
+	char folderName[] = { "C:\\Program Files\\电脑锁" };//创建文件夹的路径
 	char buffer[MAXPATH];//储存当前程序路径
-	char path[MAXPATH] = { "\\电脑锁.exe\" C:\\ProgramData /y" };//目标文件夹路径
-	char *pclock = { "C:\\ProgramData\\电脑锁.exe" };//注册表设置自启动路径
+	
+	//注册表自启动路径以及要复制到的路径
+	char *pc_lock = { "C:\\Program Files\\电脑锁\\电脑锁.exe" };  
 	
 	HKEY hKey;
 	int i;
-	system("title 添加自启动 && mode con cols=33 lines=10"); 
-	printf("\n\t输入1添加自启动\n\t输入2查看使用帮助\n\t输入3删除自启动\n\n");
+	
+	_getcwd(buffer, MAXPATH);//获取当前程序所在目录
+		
+	printf("\n\t  输入1添加自启动\n\n\t  输入2查看使用帮助\n\n\t  输入3删除自启动\n\n");
 	printf("请输入序号：");
 	while (scanf_s("%d", &i) != 1)//处理字符输入
 	{
 		printf("\n输入错误！请重试：");
-		rewind(stdin);
+		flush(); 
 	}
-	rewind(stdin);
+	flush(); 
 	
-	switch (i)
+	
+	if(i == 1)
 	{
-	case 1:
-	{
-		system("cls");
-		system("color 04");
-		printf("\n\n\n\n注意：若遭到杀软拦截，请点击允许并加入白名单。\n否则软件无法正常启动！\n\n\n");
+		system("color 04 && cls");
+		printf("\n\n注意：若遭到杀软拦截，请点击允许并加入白名单。\n否则软件无法正常启动！\n\n");
 		Sleep(3000);
-		_getcwd(buffer, MAXPATH);//获取当前程序的绝对路径
-		strcat_s(copy,sizeof(copy), buffer);//第一次合并数组
-		strcat_s(copy, sizeof(copy), path);//第二次合并数组
-		system(copy);//执行复制文件命令
+		
+		if (_access(folderName, 0))//判断文件夹是否存在
+			if (_mkdir(folderName))	   //不存在则创建文件夹
+			{
+				MessageBox(NULL, TEXT("创建目录失败！请以管理员权限重试！"), TEXT("ERROR"), MB_OK | MB_ICONERROR);
+				exit(EXIT_FAILURE);
+			}
+
+		strcat_s(buffer, MAXPATH, "\\电脑锁.exe");//复制前的路径
+		copyfile(buffer, pc_lock);//复制文件
 
 		//打开注册表启动项 
 		if (RegOpenKeyEx(HKEY_CURRENT_USER, szSubKey, 0, KEY_ALL_ACCESS, &hKey) == ERROR_SUCCESS)
 		{  //添加一个子Key,并设置值
-			RegSetValueEx(hKey, "电脑锁", 0, REG_SZ, (BYTE *)pclock, strlen(pclock));
+			RegSetValueEx(hKey, "电脑锁", 0, REG_SZ, (BYTE *)pc_lock, strlen(pc_lock));
 			RegCloseKey(hKey);//关闭注册表
-			printf("\t\t添加成功！\n\n\t");
+			printf("\t    添加成功！\n\n\t  ");
 			system("pause"); 
-			return 0;
 		}
 		else
 		{
@@ -51,20 +64,87 @@ int main(void)
 			system("pause"); 
 			return -1;
 		}
-		break;
+	
 	}
 
-	case 2:
+	else if(i == 2)
 		MessageBox(NULL, TEXT("1、设置开机自动运行前请以管理员权限运行本软件防止权限不够造成失败。\n2、设置开机自动运行前请确保“加入自启动.exe”和“电脑锁.exe”在同一目录。\n3、如果开机自启失效，请在杀毒软件里加入自启动白名单并且重新设置自启动。"), TEXT("使用帮助"), MB_OK);
-		break;
 
-	case 3://从文件夹删除文件
+	else if(i == 3)//从文件夹删除文件
 	{
-		system("del \"C:\\ProgramData\\电脑锁.exe\"");
+		system("cls && title 删除自启动");
+		printf("\n\n\n\n注意：若遭到杀软拦截，请点击允许并加入白名单！\n\n\n");
+		Sleep(3000);
+		system("cls && mode con cols=53 lines=20 && title 正在删除...");
+
+		//结束进程
+		printf("\n正在结束电脑锁的进程...\n");
+		Sleep(2000);
+		system("taskkill /f /t /im 电脑锁.exe");
+		
+		//删除文件
+		printf("\n\n正在删除文件...");
+		if (remove(pc_lock))
+			printf("\n删除电脑锁.exe失败！\n\n");
+		else
+			printf("\n删除电脑锁.exe成功！\n\n");
+		
+		//删除目录
+		printf("\n正在删除安装目录...\n");
+		system("rd /s /q C:\\\"Program Files\"\\电脑锁");
+		
+		//删除注册表
+		printf("\n\n正在删除电脑锁注册表启动项...\n");
+		system("reg delete \"HKEY_CURRENT_USER\\Software\\Microsoft\\Windows\\CurrentVersion\\Run\"  /v 电脑锁 /f");
+		
+		putchar('\n');
 		system("pause");
-		break;
 	}
-	default:printf("\n\t\t 输入错误！"); Sleep(1000); break;
+	else
+	{
+		printf("\n\t\t 输入错误！"); 
+		Sleep(1000); 
 	}
+}
+	
 	return 0;
 } 
+
+//复制文件
+void copyfile(char *fileread, char *filewrite)
+{
+	FILE *fpread;   //复制前文件的路径
+	FILE *fpwrite;  //复制后文件的路径
+	errno_t err;
+	int ch; 
+	
+	
+	if ((err = fopen_s(&fpread, fileread, "rb")) != 0 )//以二进制方式打开文件
+	{
+		MessageBox(NULL, TEXT("无法打开文件！\n请检查“电脑锁.exe”和“加入自启动.exe”是否在同一目录。\n"), TEXT("ERROR"), MB_OK | MB_ICONERROR);
+		exit(EXIT_FAILURE);
+	}
+	if ((err = fopen_s(&fpwrite, filewrite, "wb")) != 0)
+	{
+		MessageBox(NULL, TEXT("无法复制文件！请以管理员权限重试！\n"), TEXT("ERROR"), MB_OK | MB_ICONERROR);
+		exit(EXIT_FAILURE);
+	}
+
+	do
+	{
+		ch = fgetc(fpread);
+		fputc(ch, fpwrite);
+	} while (ch != EOF);
+
+	fclose(fpread);//关闭文件
+	fclose(fpwrite);
+}
+
+
+//清除缓冲区
+void flush(void)
+{
+	int ch;
+	while ((ch = getchar()) != '\n' && ch != EOF)
+		;
+}
--- 电脑锁/电脑锁.c
+++ 电脑锁/电脑锁.c
@@ -5,12 +5,12 @@
 #include <time.h>
 #include <conio.h>
 
-int  GetTime(int n);
+int  getTime(int n);
 
 int main(void)
 {
 	char Pass[7] = "115-56";
-	char TmpPass[7] = { 0 };
+	char tmpPass[7] = { 0 };
 	int i, ch;
 
 	system("title 本电脑需要输入正确的密码才能正常使用 && color 06 && mode con cols=91 lines=30 && shutdown -s -t 300");//设置窗口标题、字体颜色、窗口大小
@@ -25,10 +25,12 @@ int main(void)
 	printf("注意：如果您急需使用本电脑可以联系我的QQ：2641015842 （备注：电脑密码） \n\t\t\t\t    微信：18318085358（备注：电脑密码） \n\t\t\t\t    电话：18318085358（发短信给我）");
 	printf("\n\t\t\t\t    网站：http://fuhohua.web3v.com、https://cdmxz.github.io");
 	printf("\n\n\n\n\t\t\t       按任意键打开网站获取密码\n\n\n\n\t\t\t    ");
-	_getch();
+	ch = _getch();
+
 	system("start http://fuhohua.web3v.com");
 	printf("   按任意键进入输入密码界面");
-	_getch();
+	ch = _getch();
+
 	system("cls  && shutdown -a && shutdown -s -t 60");
 
 
@@ -36,13 +38,14 @@ int main(void)
 	{
 		printf("\n\t\t\t请在60秒内输入密码，否则电脑将会关机");
 		printf("\n\n请输入密码（你还有%d次机会）：", i);
-		scanf_s("%s", TmpPass, 7);
+		scanf_s("%s", tmpPass, 7);
 		while ((ch = getchar()) != '\n' && ch != EOF)//吸收多余字符和换行符
 			;
 
-		if (!strcmp(Pass, TmpPass))//对比密码是否正确
+
+		if (!strcmp(Pass, tmpPass))//对比密码是否正确
 		{
-			GetTime(0);
+			getTime(0);
 			system("shutdown -a");
 			system("cls && color 02 && shutdown -s -t 2400");//清屏、设置字体颜色、设置40分后关机
 			printf("\n\n\n\n\n\n\n\n\n\n\n\n\t\t\t恭喜您，密码正确，您可以使用本电脑40分钟。\n\n\n\n\n\t\t\t\t   ");
@@ -62,7 +65,7 @@ int main(void)
 		}
 		else
 		{
-			GetTime(1);
+			getTime(1);
 			printf("\n\n\t\t\t密码错误！");
 			Sleep(1000);
 		}
@@ -75,12 +78,12 @@ int main(void)
 }
 
 
-int GetTime(int n)//获取当前时间
+int getTime(int n)//获取当前时间
 {
 	errno_t err;
-	FILE *fpwrite;
+	FILE *fpWrite;
 
-	if ((err = fopen_s(&fpwrite, "log.dat", "a+")) != 0) //以只写形式打开记录当前时间的文件,并且判断文件是否正常打开
+	if ((err = fopen_s(&fpWrite, "log.dat", "a+")) != 0) //以只写形式打开记录当前时间的文件,并且判断文件是否正常打开
 		return 1;//如果文件创建错误，就返回主函数
 
 	//写入当前时间到文件
@@ -89,13 +92,16 @@ int GetTime(int n)//获取当前时间
 	time(&now);
 	localtime_s(&t, &now);
 	
+	if (fpWrite == 0)
+		return 1;
+
 	//写入日志
 	if (n == 0)
-		fprintf(fpwrite, "\n\n开机时间：%d年%02d月%02d日%02d点%02d分%02d秒。\n结果：密码输入正确！", t.tm_year + 1900, t.tm_mon + 1, t.tm_mday, t.tm_hour, t.tm_min, t.tm_sec); //写入当前年月日时分到文件
+		fprintf(fpWrite, "\n\n开机时间：%d年%02d月%02d日%02d点%02d分%02d秒。\n结果：密码输入正确！", t.tm_year + 1900, t.tm_mon + 1, t.tm_mday, t.tm_hour, t.tm_min, t.tm_sec); //写入当前年月日时分到文件
 	else if (n == 1)
-		fprintf(fpwrite, "\n\n\n开机时间：%d年%02d月%02d日%02d点%02d分%02d秒。\n结果：密码输入错误！", t.tm_year + 1900, t.tm_mon + 1, t.tm_mday, t.tm_hour, t.tm_min, t.tm_sec); //写入当前年月日时分到文件
+		fprintf(fpWrite, "\n\n\n开机时间：%d年%02d月%02d日%02d点%02d分%02d秒。\n结果：密码输入错误！", t.tm_year + 1900, t.tm_mon + 1, t.tm_mday, t.tm_hour, t.tm_min, t.tm_sec); //写入当前年月日时分到文件
 
-	fclose(fpwrite); //关闭文件
+	fclose(fpWrite); //关闭文件
 	
 	return 0;
 }
